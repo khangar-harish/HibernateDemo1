fc866c2739df77afdf3043549ff5ac0d26687238
fhdz4n <fhdz4n@pl1inpun1213nb.net.plm.eds.com>
2019-11-07 01:50:03 +0530

    for inheritance

---
+++ HibernateInheritance/src/com/hash/CollegeStudent.java
@@ -0,0 +1,27 @@
+package com.hash;
+
+public class CollegeStudent {
+
+	private String university;
+	private String trade;
+	
+	public CollegeStudent() {
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getTrade() {
+		return trade;
+	}
+
+	public void setTrade(String trade) {
+		this.trade = trade;
+	}
+	
+}
---
+++ HibernateInheritance/src/com/hash/SchoolStudent.java
@@ -0,0 +1,25 @@
+package com.hash;
+
+public class SchoolStudent extends Student{
+	
+	private String board;
+	private String type;
+	
+	
+	public SchoolStudent() {
+	}
+	public String getBoard() {
+		return board;
+	}
+	public void setBoard(String board) {
+		this.board = board;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
